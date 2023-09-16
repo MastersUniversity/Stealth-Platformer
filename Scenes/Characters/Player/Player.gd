100b8f5a6302a7ffea006600e6fafac8002ecbae
lukecord <61397303+lukecord@users.noreply.github.com>
2023-09-16 00:24:20 -0700

    Crouch Feature Part 1
    Rough version of the crouch feature

--- Scenes/Characters/Player/Player.gd
+++ Scenes/Characters/Player/Player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+const WALK_SOUND = 3.0
+const CROUCH_SPEED = 50.0
+const CROUCH_SOUND = 1.5
 const JUMP_VELOCITY = -300.0
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var current_speed = SPEED
+var current_sound = 0
 
 
 func _physics_process(delta):
@@ -19,25 +24,37 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	# Handles making the sound based on what the player is doing
-	# (Could use refinement)
-	if velocity.x != 0:
-		$Sound.sound_radius = 3
-		$Sound.set_global_location_vector(self.global_position)
-	elif velocity.y < JUMP_VELOCITY * .9:
-		$Sound.sound_radius = 3
+	if Input.is_key_pressed(KEY_S):
+		current_speed = CROUCH_SPEED
+		current_sound = CROUCH_SOUND
+		$Sprite2D.scale.y = 0.5
+		$WalkingColider.disabled = true
+		$CrouchingCollider.disabled = false
+		
 	else:
-		$Sound.sound_radius = 0
+		current_speed = SPEED
+		current_sound = WALK_SOUND
+		$Sprite2D.scale.y = 1
+		$WalkingColider.disabled = false
+		$CrouchingCollider.disabled = true
+		
+	# Handles making the sound based on what the player is doing
+	if velocity.y < JUMP_VELOCITY * .9:
+		current_sound = WALK_SOUND
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED/10)
+		velocity.x = move_toward(velocity.x, direction * current_speed, current_speed/10)
+		$Sound.sound_radius = current_sound
+		$Sound.set_global_location_vector(self.global_position)
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$Sound.sound_radius = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED/30)
+		$Sound.sound_radius = 0
 	
 	
 	move_and_slide()
