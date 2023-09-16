217e9a17c32403c610b373ff15d4573bd3e89a41
Ian_Estrella <eianestrella7@gmail.com>
2023-09-16 00:09:48 -0700

    Programming flipping of Civilians with sound
    Gotta debug because civilians perform spinjutsu off the map lol

--- Scenes/Characters/Civilian/Civilian.gd
+++ Scenes/Characters/Civilian/Civilian.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var pursuing = false
+var faceLeft = true
 @export var player = Node2D
 
 func _physics_process(delta):
@@ -16,7 +17,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	
 	if pursuing:
-		velocity.x = move_toward(velocity.x, -SPEED, 1)
+		if faceLeft:
+			scale.x = 1
+			velocity.x = move_toward(velocity.x, -SPEED, 1)
+		else:
+			scale.x = -1
+			velocity.x = move_toward(velocity.x, SPEED, 1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 2)
 	
@@ -31,3 +37,17 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body == player:
 		pursuing = false
+
+
+func _on_sound_listener_heard_sound(body):
+	pursuing = true
+	var soundLocation = body.get_global_location()
+	check_direction(soundLocation)
+		
+func check_direction(soundLocation):
+	if soundLocation.x < transform.x.x:
+		faceLeft = true
+	elif soundLocation.x > transform.x.x:
+		faceLeft = false
+		
+	
